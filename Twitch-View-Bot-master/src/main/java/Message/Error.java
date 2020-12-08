eaf6104fa5044b61b05cbd981d38a2ae7ae5f5f4
WassInfinity <64957549+WassInfinity@users.noreply.github.com>
2020-12-08 15:24:37 +0100

    Update Error.java

--- Twitch-View-Bot-master/src/main/java/Message/Error.java
+++ Twitch-View-Bot-master/src/main/java/Message/Error.java
@@ -1,5 +1,5 @@
 package Message;
 
 public class Error {
-    public static final String PROXY_FILE_NOT_FOUND = "Proxy file has not been found. Try again";
+    public static final String PROXY_FILE_NOT_FOUND = "Le fichier proxy n'a pas été trouvé. Réessayer";
 }
